10d3fa72353d2c42ac424c00693f5735ec8ed4ea
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2019-02-01 21:28:34 +0900

    Add UndoRedoCountTests

---
+++ EditingSystem/EditingSystem.Tests/UndoRedoCountTests.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+
+namespace EditingSystem.Tests
+{
+    public class UndoRedoCountTests
+    {
+        [Fact]
+        public void Basic()
+        {
+            var history = new History();
+            var model = new TestModel(history);
+
+            Assert.Equal((0, 0), history.UndoRedoCount);
+
+            model.IntValue = 123;
+            Assert.Equal((1, 0), history.UndoRedoCount);
+
+            model.IntValue = 456;
+            Assert.Equal((2, 0), history.UndoRedoCount);
+
+            history.Undo();
+            Assert.Equal((1, 1), history.UndoRedoCount);
+
+            history.Clear();
+            Assert.Equal((0, 0), history.UndoRedoCount);
+        }
+
+        public class TestModel : EditableModelBase
+        {
+            public TestModel(History history)
+            {
+                SetupEditingSystem(history);
+            }
+
+            #region IntValue
+
+            private int _IntValue;
+
+            public int IntValue
+            {
+                get => _IntValue;
+                set => SetEditableProperty(v => _IntValue = v, _IntValue, value);
+            }
+
+            #endregion
+        }
+    }
+}
