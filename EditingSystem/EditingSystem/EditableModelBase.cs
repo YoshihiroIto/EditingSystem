2d3933f64ac4a1c9565a02ab1feedf32b32348b7
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2019-02-22 10:38:19 +0700

    Add SetPropertyWithoutHistory, SetFlagPropertyWithoutHistory

--- EditingSystem/EditingSystem/EditableModelBase.cs
+++ EditingSystem/EditingSystem/EditableModelBase.cs
@@ -88,6 +88,42 @@ namespace EditingSystem
             return true;
         }
 
+        #region Without History
+        protected bool SetPropertyWithoutHistory<T>(ref T storage, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+                return false;
+
+            storage = value;
+
+            RaisePropertyChanged(propertyName);
+
+            return true;
+        }
+
+        protected bool SetFlagPropertyWithoutHistory(ref uint storage, uint flag, bool value, [CallerMemberName] string propertyName = "")
+        {
+            if (value)
+            {
+                if ((storage & flag) != 0)
+                    return false;
+
+                storage |= flag;
+            }
+            else
+            {
+                if ((storage & flag) == 0)
+                    return false;
+
+                storage &= ~flag;
+            }
+
+            RaisePropertyChanged(propertyName);
+
+            return true;
+        }
+        #endregion
+
         #region INotifyPropertyChanged
 
         public event PropertyChangedEventHandler PropertyChanged;
