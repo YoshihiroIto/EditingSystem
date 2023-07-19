b4ca86944e140829254f6c42537adb5cbe2749ad
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2023-07-19 21:42:25 +0900

    Update HistoryGenerator

--- EditingSystem/Jewelry.EditingSystem.Tests/TestModels/CommunityToolkitMvvmBasicTestModel.cs
+++ EditingSystem/Jewelry.EditingSystem.Tests/TestModels/CommunityToolkitMvvmBasicTestModel.cs
@@ -35,4 +35,16 @@ public sealed partial class CommunityToolkitMvvmBasicTestModel : ObservableObjec
     partial void OnStringValueChanged(string value) => ++ChangingCount;
     partial void OnIntCollectionChanged(ObservableCollection<int> value) => ++ChangingCount;
     partial void OnCollectionChanged(ObservableCollection<CollectionItem> value) => ++ChangingCount;
+
+    partial void OnCollectionChanging(ObservableCollection<CollectionItem>? oldValue, ObservableCollection<CollectionItem> newValue)
+        => ES_OnCollectionChanging(oldValue, newValue);
+
+    partial void OnIntCollectionChanging(ObservableCollection<int>? oldValue, ObservableCollection<int> newValue)
+        => ES_OnIntCollectionChanging(oldValue, newValue);
+
+    partial void OnIntValueChanging(int oldValue, int newValue)
+        => ES_OnIntValueChanging(oldValue, newValue);
+
+    partial void OnStringValueChanging(string? oldValue, string newValue)
+        => ES_OnStringValueChanging(oldValue, newValue);
 }
