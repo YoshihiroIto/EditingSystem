e74be6bcf394b20924b65ea178714390d90662cd
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2026-08-30 18:25:43 +0900

    test: strengthen weak collection listener lifetime coverage

--- EditingSystem/Jewelry.EditingSystem.Tests/Tests/CollectionChangedWeakEventManagerTests.cs
+++ EditingSystem/Jewelry.EditingSystem.Tests/Tests/CollectionChangedWeakEventManagerTests.cs
@@ -29,6 +29,46 @@ public sealed class CollectionChangedWeakEventManagerTests
     }
 
     [Fact]
+    public void Registration_keeps_handler_alive()
+    {
+        using var manager = new CollectionChangedWeakEventManager();
+        var source = new ObservableCollection<int> { 1 };
+        var counter = new NotificationCounter();
+        var handlerReference = RegisterTemporaryHandler(manager, source, counter);
+
+        CollectGarbage();
+
+        Assert.True(handlerReference.IsAlive);
+        source.Add(2);
+        Assert.Equal(1, counter.Count);
+    }
+
+    [Fact]
+    public void Dispose_unsubscribes_all_live_registrations()
+    {
+        var manager = new CollectionChangedWeakEventManager();
+        var firstSource = new ObservableCollection<int> { 1 };
+        var secondSource = new ObservableCollection<int> { 10 };
+        var notificationCount = 0;
+        NotifyCollectionChangedEventHandler handler = (_, _) => ++notificationCount;
+
+        manager.AddWeakEventListener(firstSource, handler);
+        manager.AddWeakEventListener(firstSource, handler);
+        manager.AddWeakEventListener(secondSource, handler);
+
+        firstSource.Add(2);
+        secondSource.Add(20);
+        Assert.Equal(2, notificationCount);
+
+        notificationCount = 0;
+        manager.Dispose();
+
+        firstSource.Add(3);
+        secondSource.Add(30);
+        Assert.Equal(0, notificationCount);
+    }
+
+    [Fact]
     public void Removed_registration_is_not_available_for_snapshot_lookup()
     {
         using var manager = new CollectionChangedWeakEventManager();
@@ -54,6 +94,17 @@ public sealed class CollectionChangedWeakEventManagerTests
     }
 
     [MethodImpl(MethodImplOptions.NoInlining)]
+    private static WeakReference RegisterTemporaryHandler(
+        CollectionChangedWeakEventManager manager,
+        ObservableCollection<int> source,
+        NotificationCounter counter)
+    {
+        NotifyCollectionChangedEventHandler handler = counter.OnCollectionChanged;
+        manager.AddWeakEventListener(source, handler);
+        return new WeakReference(handler);
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
     private static WeakReference RegisterTemporarySource(CollectionChangedWeakEventManager manager)
     {
         var source = new ObservableCollection<int> { 1 };
@@ -69,6 +120,16 @@ public sealed class CollectionChangedWeakEventManagerTests
             GC.Collect();
             GC.WaitForPendingFinalizers();
             GC.Collect();
+        }
+    }
+
+    private sealed class NotificationCounter
+    {
+        public int Count { get; private set; }
+
+        public void OnCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            ++Count;
         }
     }
 }
